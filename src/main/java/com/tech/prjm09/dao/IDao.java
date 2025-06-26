b52faa6be83d90383c4f40610cf2efda1a7cd69b
Shrimptempura <ongtzxowns98@gmail.com>
2025-06-26 12:06:53 +0900

    maininit

---
+++ src/main/java/com/tech/prjm09/dao/IDao.java
@@ -0,0 +1,13 @@
+package com.tech.prjm09.dao;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.tech.prjm09.dto.BDto;
+
+@Mapper
+public interface IDao {
+	ArrayList<BDto> list();
+
+}
